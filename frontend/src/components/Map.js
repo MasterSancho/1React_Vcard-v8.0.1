766b0ba7dae2dddc537cbf72b8c463d1fb37d874
Alex M <1maltsevalex@gmail.com>
2022-10-23 16:09:39 +0300

    003->Map Updated

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -5,15 +5,15 @@ const Map = () => {
   <div className='leafletContainer'>
    <MapContainer
     style={{ height: '100%', width: '100%' }}
-    center={[32.1753749, 34.9254946]}
-    zoom={13}
+    center={[32.17560649008901, 34.92767793347163]}
+    zoom={15}
     scrollWheelZoom={false}>
     <TileLayer
      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
      url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
     />
 
-    <Marker position={[32.1753749, 34.9254946]}>
+    <Marker position={[32.17560649008901, 34.92767793347163]}>
      <Popup>יוחנן הסנדלר 6 כפר סב</Popup>
     </Marker>
    </MapContainer>
